969610f73eb92ce8d4475af7db0eea1592122924
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-12-04 13:27:31 -0600

    build color mode switch

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import theme from "./theme.ts";
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>,
