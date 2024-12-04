074accb3e0d58c972b05a48b9aba5cc0b0c7838d
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-12-04 12:05:45 -0600

    install chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={defaultSystem}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
