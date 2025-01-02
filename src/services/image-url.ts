13566ea319f0f83f24238a934cd7e3e896542dfa
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-01-02 13:30:56 -0800

    Update: img service

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImg from "../assets/no-img.webp"
+
 const getCroppedImgUrl = (url: string) => {
-    if(!url) return;
+    if(!url) return noImg;
     const idx = url.indexOf("media/") + "media/".length;
     return url.slice(0, idx) + "crop/600/400/" + url.slice(idx);
 }
